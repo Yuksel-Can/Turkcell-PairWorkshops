7a7998941e3782ba27a74963b380eb516e580769
Yüksel Can Özdemir <yukselcanozdemir@gmail.com>
2022-02-09 01:52:20 +0300

    Amicable Number algorithm

---
+++ Day2/lesson2Pair9Workshop3/src/lesson2Pair9Workshop3/Main.java
@@ -0,0 +1,8 @@
+package lesson2Pair9Workshop3;
+
+public class Main {
+	public static void main(String[] args) {
+		//Proje = Ýlk iki Arkadaþ Sayýnýn elde edilmesi(Arkadaþ Sayý=Kendi hariç bölenleri toplamý birbirine(ayný deðil) eþit olan sayýlardýr)
+
+	}
+}
