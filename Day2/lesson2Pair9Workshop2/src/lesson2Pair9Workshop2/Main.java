f623ea92904a6f13e63abb5e522732e89993e93b
Yüksel Can Özdemir <yukselcanozdemir@gmail.com>
2022-02-08 18:29:34 +0300

    Perfect Number algorithm

---
+++ Day2/lesson2Pair9Workshop2/src/lesson2Pair9Workshop2/Main.java
@@ -0,0 +1,8 @@
+package lesson2Pair9Workshop2;
+
+public class Main {
+	public static void main(String[] args) {
+		//Proje = Ýlk 4 Süper Sayý'nýn elde edilmesi(Süper Sayý = Bölenleri toplamý kendisine eþit olan sayýdýr) 
+	}
+
+}
